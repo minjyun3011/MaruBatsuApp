8cf813c7fa13814c6d12348c799e425c457592ae
minjyun3011 <grand3canyon10@gmail.com>
2023-09-05 16:06:38 +0900

    Initial Commit

---
+++ MaruBatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaruBatsuApp
+//
+//  Created by 佐藤壮 on 2023/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
